2c37c905f120f60a85dfcefd9552241f27a995d8
German <cisnerosgd@gmail.com>
2018-07-25 16:05:45 -0300

    subo proyecto

---
+++ src/main/java/com/helpVeterinary/helpveterinaryfe/HelpVeterinaryFeApplication.java
@@ -0,0 +1,12 @@
+package com.helpVeterinary.helpveterinaryfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpVeterinaryFeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpVeterinaryFeApplication.class, args);
+	}
+}
