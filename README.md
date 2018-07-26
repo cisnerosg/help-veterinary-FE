ffdf5ee999cb0b39666f0191be8b1c0ab795961d
German <cisnerosgd@gmail.com>
2018-07-26 12:43:57 -0300

    agrego login

--- README.md
+++
@@ -1,2 +0,0 @@
-# help-veterinary-fe
-help-veterinary-fe
---
+++ src/main/java/com/helpVeterinary/fe/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package com.helpVeterinary.fe.config;
+
+import com.helpVeterinary.fe.service.UserDetailsServiceImpl;
+import com.helpVeterinary.fe.web.LoggingAccessDeniedHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private LoggingAccessDeniedHandler accessDeniedHandler;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers(
+                            "/",
+                            "/js/**",
+                            "/css/**",
+                            "/img/**",
+                            "/webjars/**").permitAll()
+                    .antMatchers("/user/**").hasRole("USER")
+                    .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                .and()
+                .logout()
+                    .invalidateHttpSession(true)
+                    .clearAuthentication(true)
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .logoutSuccessUrl("/login?logout")
+                    .permitAll()
+                .and()
+                .exceptionHandling()
+                    .accessDeniedHandler(accessDeniedHandler);
+    }
+
+    @Bean
+	  public UserDetailsService userDetailsService() {
+	    return new UserDetailsServiceImpl();
+	  };
+	  
+	  @Bean
+	  public BCryptPasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
+	  };
+	  
+	  @Override
+	  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	    auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+	  }
+
+}
